ba8216b84281aafe78f65655f7939fb17c65a511
Konsta Poikolainen <ag1539@student.jamk.fi>
2025-12-17 23:03:40 +0200

    muokattu vitest.config

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['tests2/**/*.test.{js,ts}'], // ajetaan vain tests2-kansion testit
-    exclude: ['**/node_modules/**'],        // normaali ignore
+    include: ['public/tests2/**/*.test.{js,ts}'], // oikea polku
+    exclude: ['**/node_modules/**'],
   },
 })
