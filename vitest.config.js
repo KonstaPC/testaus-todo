25db558275405795fa118311d0c22e8a34b30192
Konsta Poikolainen <ag1539@student.jamk.fi>
2026-03-09 13:03:45 +0200

    Muokattu vitest.config.js, jotta ravintolatehtävän yksikkötestit voidaan ajaa

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['public/tests2/**/*.test.{js,ts}'], // vain tests2
-    exclude: ['**/node_modules/**', '**/public/other-tests/**'], // muut pois
+    include: ['public/tests2/**/*.test.{js,ts}', 'ravintolateht/tests/**/*.test.{js,ts}'], // vain tests2
+    exclude: ['**/node_modules/**'], // muut pois
   },
 })
